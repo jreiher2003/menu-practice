e2ffd1f5979fff8a717db48e95af66ddc5c62d04
Jeff Reiher <jeffreiher@gmail.com>
2015-11-18 09:55:11 -0500

    made a query to db and created a for loop var which i put into my template to list out each menuitem by place

--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,10 @@
 from flask import render_template
 from menu import app, db
-from models import Place
+from models import Place, MenuItem
 
 @app.route('/')
 @app.route('/hello')
 def index():
     place = db.session.query(Place).all()
-    return render_template('base.html', place=place)
+    items = db.session.query(Place,MenuItem).filter(Place.id==MenuItem.place_id).all()
+    return render_template('base.html', place=place, items=items)
