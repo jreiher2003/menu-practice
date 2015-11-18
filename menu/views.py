2e25224fcbc72d2aca3ecbae762a67a91813f392
Jeff Reiher <jeffreiher@gmail.com>
2015-11-18 09:12:29 -0500

    simplified flask my importing an instance of sqlalachemy

--- menu/views.py
+++ menu/views.py
@@ -2,15 +2,6 @@ from flask import render_template
 from menu import app, db
 from models import Place
 
-
-# from sqlachemy import create_engine 
-# from sqlalchemy.orm import sessionmaker
-# from models import Base, Place
-
-# engine = create_engine('sqlite:///menu.db')
-# Base.metadata.bind = engine 
-
-
 @app.route('/')
 @app.route('/hello')
 def index():
