01fb18667e70c0f7f597146d856a5f19a92d52a7
Jeff Reiher <jeffreiher@gmail.com>
2015-11-18 10:41:12 -0500

    add var to routes in flask

--- menu/views.py
+++ menu/views.py
@@ -3,8 +3,8 @@ from menu import app, db
 from models import Place, MenuItem
 
 @app.route('/')
-@app.route('/hello')
-def index():
-    place = db.session.query(Place).all()
-    items = db.session.query(Place,MenuItem).filter(Place.id==MenuItem.place_id).all()
+@app.route('/place/<int:place_id>/')
+def index(place_id):
+    place = db.session.query(Place).filter_by(id = place_id).one()
+    items = db.session.query(MenuItem).filter_by(place_id=place_id)
     return render_template('base.html', place=place, items=items)
